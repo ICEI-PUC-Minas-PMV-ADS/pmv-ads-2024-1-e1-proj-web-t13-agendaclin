5b25034de94da38190956980a212fb367bdf4b6d
viniciusmachadolima <166730468+viniciusmachadolima@users.noreply.github.com>
2024-06-06 09:17:23 -0300

    vinicius 06/06 pagina medico

--- codigo-fonte/modules/doctor-professional-profile/doctor-professional-profile-service.js
+++ codigo-fonte/modules/doctor-professional-profile/doctor-professional-profile-service.js
@@ -1,5 +1,49 @@
-export class DoctorProfessionalProfileService {
-    getData() {
-        return Promise.resolve(['Item 1', 'Item 2', 'Item 3']);
-    }
-}
+//export class DoctorProfessionalProfileService {
+   // getData() {
+    //    return Promise.resolve(['Item 1', 'Item 2', 'Item 3']);
+  //  }
+//}
+//botao salvar
+ const salvar = document.getElementById("salvar")
+ //salva os dados
+ function salvarDados()
+ {
+    const endereço = document.getElementById("endereço").Value;
+    const numero = document.getElementById("numero").Value;
+    const sobreMim = document.getElementById("sobreMim").Value;
+    const experiencia = document.getElementById("experiencia").Value;
+    const formaçao = document.getElementById("formaçao").Value;
+
+
+    localStorage.setItem("endereço" , endereço)
+    localStorage.setItem("numero" , numero)
+    localStorage.setItem("sobreMim" , sobreMim)
+    localStorage.setItem("experiencia" , experiencia)
+    localStorage.setItem("formaçao" , formaçao)
+ }
+//verifica se os dados estao preenchidos
+ function verificarInput()
+ {
+    const endereço = document.getElementById("endereço")
+    const numero = document.getElementById("numero")
+    const sobreMim = document.getElementById("sobreMim")
+    const experiencia = document.getElementById("experiencia")
+    const formaçao = document.getElementById("formaçao")
+
+    if(endereço === "" || numero === "" || sobreMim === "" || experiencia === "" || formaçao === "" ) 
+        {
+            alert("por favor, certifique-se se está preenchido corretamente.");
+            return false;
+        }
+        return true;
+ }
+//evento click no botao, para ele ir para a proxima tela
+ salvar.addEventListener("click", function()
+ {
+    if (verificarInput()) 
+        {
+            salvarDados();
+
+            window.location.href = "../schedule-consult-step-2/schedule-consult-step-2.html"
+        }
+ });
