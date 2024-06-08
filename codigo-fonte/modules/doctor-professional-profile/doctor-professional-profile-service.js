d4aa60f2ffc12e0b71e38f50b33e29e9545322c2
viniciusmachadolima <166730468+viniciusmachadolima@users.noreply.github.com>
2024-06-08 03:28:57 -0300

    08/06 pagina paciente

--- codigo-fonte/modules/doctor-professional-profile/doctor-professional-profile-service.js
+++ codigo-fonte/modules/doctor-professional-profile/doctor-professional-profile-service.js
@@ -1,5 +1,31 @@
-export class DoctorProfessionalProfileService {
-    getData() {
-        return Promise.resolve(['Item 1', 'Item 2', 'Item 3']);
-    }
+//export class DoctorProfessionalProfileService {
+   // getData() {
+    //    return Promise.resolve(['Item 1', 'Item 2', 'Item 3']);
+  //  }
+//}
+ const pesquisa = document.getElementById("pesquisa")
+//pesquisa
+ function.pesquisar()
+{
+    const doença = document.getElementById("doença").value;
+    const filtro = document.getElementById("filtro").value;
+
+    localStorage.setItem("doença" , doença)
+    localStorage.setItem("filtro" , filtro)
+}
+
+pesquisa.addEventListener("click", function()
+{
+    if (pesquisar())
+        {
+            window.location.href = "../schedule-consult-step-2/schedule-consult-step-2.html"
+        }
+});
+
+const logo = document.getElementById("logo")
+
+function.retornar()
+{
+    logo.addEventListener("click", function())
+    
 }
