d5554e5e9582094d7f1a79beceffd27d4ba0dd9f
viniciusmachadolima <166730468+viniciusmachadolima@users.noreply.github.com>
2024-06-08 16:58:42 -0300

    08/06 pagina paciente.js vinicius

--- codigo-fonte/modules/doctor-professional-profile/doctor-professional-profile-service.js
+++ codigo-fonte/modules/doctor-professional-profile/doctor-professional-profile-service.js
@@ -3,29 +3,18 @@
     //    return Promise.resolve(['Item 1', 'Item 2', 'Item 3']);
   //  }
 //}
- const pesquisa = document.getElementById("pesquisa")
-//pesquisa
- function.pesquisar()
-{
-    const doença = document.getElementById("doença").value;
-    const filtro = document.getElementById("filtro").value;
-
-    localStorage.setItem("doença" , doença)
-    localStorage.setItem("filtro" , filtro)
-}
-
-pesquisa.addEventListener("click", function()
-{
-    if (pesquisar())
-        {
-            window.location.href = "../schedule-consult-step-2/schedule-consult-step-2.html"
-        }
-});
 
 const logo = document.getElementById("logo")
 
-function.retornar()
+logo.addEventListener("click", function()
 {
-    logo.addEventListener("click", function())
-    
+   window.location.href = "../../index.html"
+})
+
+const hora = document.getElementById("hora")
+
+hora.addEventListener("click", function()
+{
+   window.location.href = "../schedule-consult-step-1/schedule-consult-step-1.html"
 }
+)
