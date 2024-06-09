f63ff89c8a75c80df5a3c09f60083cdc9429fe8b
Giovanni Gonçalves Shintaku <166865238+GiovanniShintaku@users.noreply.github.com>
2024-06-09 17:47:13 -0300

    Update doctor-signup-step-1-service.js

--- codigo-fonte/modules/doctor-signup-step-1/doctor-signup-step-1-service.js
+++ codigo-fonte/modules/doctor-signup-step-1/doctor-signup-step-1-service.js
@@ -1,5 +1,36 @@
-export class DoctorSignupStep1Service {
-    getData() {
-        return Promise.resolve(['Item 1', 'Item 2', 'Item 3']);
+/*
+//adquirindo os dados do que foi inserido nos campos input
+
+const option = document.querySelector("input[name = 'option']:checked");
+const specialty = document.getElementById('specialty');
+const userName = document.getElementById('name');
+const userSurname = document.getElementById('surname');
+
+//criando a classe que vai instanciar um objeto com os dados passados 
+ export class user {
+    opcaoRadio = document.querySelector("input[name = 'option']:checked").value;
+    especialidade = document.getElementById('specialty').innerHTML;
+    nome = document.getElementById('name').innerHTML;
+    sobrenome = document.getElementById('surname').innerHTML;
+    constructor (){
+        this.opcaoRadio = opcaoRadio.value;
+        this.especialidade = especialidade.innerHTML;
+        this.nome = nome.innerHTML;
+        this.sobrenome = sobrenome.innerHTML;
+    }
+
+}
+*/
+export class docCad {
+    authenticate(option, specialty, userName, userSurname) {
+        // Simular autenticação
+        return new Promise((resolve, reject) => {
+            if (option === "Médico / especialista" || option === "Clínica" && specialty === "111", userName === "222", userSurname === "333") {
+                resolve("Login successful!");
+            } else {
+                reject("Invalid credentials");
+            }
+        });
     }
 }
+
