e87e0b539587b53e470775eb536589e9bda52ac9
Giovanni Gonçalves Shintaku <166865238+GiovanniShintaku@users.noreply.github.com>
2024-06-21 16:55:59 -0300

    Update doctor-signup-step-2-controller.js

--- modules/doctor-signup-step-2/doctor-signup-step-2-controller.js
+++ modules/doctor-signup-step-2/doctor-signup-step-2-controller.js
@@ -1,4 +1,5 @@
 import { DoctorSignupStep2Service } from './doctor-signup-step-2-service.js';
+import { emailSignUp } from '/js/shared/auth.js';
 
 export class DoctorSignupStep2Controller {
     constructor() {
@@ -7,31 +8,43 @@ export class DoctorSignupStep2Controller {
     }
 
     setData() {
-        document.getElementById('signUp').addEventListener('submit', (e) => {
+        document.getElementById('signUp').addEventListener('submit', async (e) => {
             e.preventDefault();
             const city = document.getElementById("city").value;
             const dde = document.getElementById('DDE').value;
             const phone = document.getElementById('phone').value;
+
             const email = document.getElementById('email').value;
             const password = document.getElementById("password").value;
-            
-            const userData = { city, dde, phone, email, password };
-            localStorage.setItem('userDetails', JSON.stringify(userData));
+            try {
+                const signup = await emailSignUp(email, password);
+                //validando os outros campos
+                this.login(city, dde, phone);
+                //guardando no localStorage
+                const userData = { city, dde, phone, signup};
+                localStorage.setItem('userDetails', JSON.stringify(userData));
+                // Redirecionar para a próxima página após login bem-sucedido
+                document.getElementById('closeModalButton').addEventListener('click', function() {
+                    window.location.href = "/#/home.html";
+                });
 
-            this.login(city, dde, phone, email, password);
+                document.getElementById('closeModalicon').addEventListener('click', function() {
+                    window.location.href = '/#/home.html';
+                });
+
+            } catch(error){
+                console.error('Erro ao fazer cadastro:', error.message);
+                alert(`Erro ao criar usuário: ${error.message}`);
+            }   
+            
         });
     }
 
-    login(city, dde, phone, email, password) {
-        this.homeService.authenticate(city, dde, phone, email, password).then(message => {
-            document.getElementById('messageBox').innerText = message;
-            console.log(message);
-            // Redirecionar para a próxima página após login bem-sucedido
-            window.location.href = "/home.html"; 
-        }).catch(err => {
-            document.getElementById('messageBox').innerText = 'Login failed';
-            console.error(err);
-        });
+    login(city, dde, phone) {
+        this.homeService.authenticate(city, dde, phone)
+            console.log("login autenticado");
+            
+
     }
 }
 
